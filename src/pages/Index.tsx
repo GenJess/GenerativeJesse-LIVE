5012241479421fc8b795fc7e97f27a64f3a6635a
GenJess <genjess.eth@gmail.com>
2025-04-30 03:12:58 -0500

    Landing page: Spline is now fixed fullscreen, no scroll, no overlays. Pure Spline viewport as requested.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function Index() {
   return (
-    <main style={{ width: '100vw', minHeight: '200vh', background: 'black' }}>
-      <Spline scene="https://prod.spline.design/wPA3LRYxI9Gxx53V/scene.splinecode" />
-    </main>
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', background: 'black', zIndex: 0 }}>
+  <Spline scene="https://prod.spline.design/wPA3LRYxI9Gxx53V/scene.splinecode" />
+</div>
   );
 }
