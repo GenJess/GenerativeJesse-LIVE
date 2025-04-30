3d3694ca2d02ba6278af94ba8bfbe0780ac5929f
GenJess <genjess.eth@gmail.com>
2025-04-30 03:22:28 -0500

    Restore scrollable layout for Spline landing: page scroll enabled, Spline fills viewport, Spline handles scroll/redirect logic.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function Index() {
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', background: 'black', zIndex: 0 }}>
+    <main style={{ width: '100vw', height: '100vh', minHeight: '200vh', background: 'black', overflow: 'hidden' }}>
   <Spline scene="https://prod.spline.design/wPA3LRYxI9Gxx53V/scene.splinecode" />
-</div>
+</main>
   );
 }
